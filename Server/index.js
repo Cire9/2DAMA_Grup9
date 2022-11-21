357214c49ff6f4316451c022824cf6c15a1b69ed
Andres <a21andmejval@inspedralbes.cat>
2022-11-21 12:55:08 +0100

    Se mete dentro de la BD

--- Server/index.js
+++ Server/index.js
@@ -1,9 +1,27 @@
 const express = require("express");
 const cors = require("cors");
+const mysql = require("mysql");
 
 const app = express();
 const PORT = 3000;
-;
+
+var bodyParser = require("body-parser");
+
+var connection = mysql.createConnection({
+    host: "labs.inspedralbes.cat",
+    database: "a21xavcabmil_proyecto",
+    user: "a21xavcabmil_user",
+    password: "Pedralbes22_23"
+})
+
+connection.connect(function(error){
+    if (error){
+        throw error;
+    }
+    else{
+        console.log('MySQL Database is connected Succesfully');
+    }
+});
 
 /*let cors_config = {
     origin: function (origin, callback){
@@ -15,18 +33,32 @@ app.use(cors(cors_config));*/
 
 app.use(cors({
     origin: function (origin, callback){
+
         return callback(null, true);
     }
 }));//Es lo mismo que lo que está arriba comentado.
 
+app.use(express.json());
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: true}));
+
 app.post ("/login", (req, res)=> {
-    console.log(req.body);
+    
     console.log('I got a request!');
     const data = req.body;
-    res.json({
-        usuari: data.usuari,
+    /*res.json({
+        email: data.email,
         passw: data.password
-    })
+    })*/
+    console.log(req.body);
+    connection.query('SELECT * FROM Persona WHERE email = "'+data.email+'" and passw = "'+data.password+'";',
+    function (err, result, fields){
+        if (err) throw err;
+        res.send(result)
+        
+    });
     /*let str = JSON.stringify(config);
 
     res.send(str);*/
--- User/index.js
+++ User/index.js
@@ -165,26 +165,56 @@ new Vue({
             return {
               valid: false,
               e1: false,
-              password: '',
+              //password: '',
               passwordRules: [
                 (v) => !!v || 'Password is required',
               ],
-              email: '',
+              //email: '',
               emailRules: [
                 (v) => !!v || 'E-mail is required',
                 (v) => /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(v) || 'E-mail must be valid'
               ],
+              date:{
+                email: '',
+                password: '',
+                }
             }
           },
     methods: {
             submit () {
-              console.log("hola")
-              if (this.$refs.form.validate()) {
-                this.$refs.form.$el.submit()
-              }
+               
+                console.log("hola");
+                console.log(this.data);
+                fetch("http://127.0.0.1:3000/login", {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json; charset=utf-8',
+                },
+                mode: 'cors',
+                body: JSON.stringify(this.date)
+                //cache: 'default'
+                }).then(
+                    (response) => {
+                        console.log(response);
+                        return response.json();
+                    }
+                ).then(
+                    (data) => {
+                        console.log(data);
+                        this.response = data;
+                    }
+                ).catch(
+                    (error) => {
+                        console.log("ERROR!!");
+                        console.log(error);
+                    }
+                )}/*
+                if (this.$refs.form.validate()) {
+                    this.$refs.form.$el.submit()
+                }
+                },
+                clear () {
+                this.$refs.form.reset()
+                }*/
             },
-            clear () {
-              this.$refs.form.reset()
-            }
-          },
   })
