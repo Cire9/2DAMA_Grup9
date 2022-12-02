e702b67dc455d688e3c4941c9a153b87a111d2de
Cire9 <96518545+Cire9@users.noreply.github.com>
2022-12-02 10:23:03 +0100

    Add files via upload

--- Pagina_Web/main.php
+++ Pagina_Web/main.php
@@ -3,9 +3,19 @@
 ?>
 <!DOCTYPE html>
 <html>
+
 <head>
-	<title>RecyclApp</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Reclicapp</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/shorthandcss@1.1.1/dist/shorthand.min.css" />
+    <link rel="stylesheet" href="css" />
+    <link rel="stylesheet" type="text/css"
+        href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.css" />
+        <link rel="shortcut icon" type="image/x-icon" href="Logo_1.png">
+ 
 </head>
+
 <body>
 
 <br>
